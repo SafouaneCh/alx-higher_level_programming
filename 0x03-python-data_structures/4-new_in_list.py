0ec0fcbd8b201de67b904da3323027b0e206cdd0
SafouaneCh <safouanecharki3@gmail.com>
2024-01-22 11:18:53 -0800

    copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def nex_in_list(my_list, idx, element):
-    new_list = my_list
     if idx < 0 or idx >= len(my_list):
-        return new_list
-    new_list[idx] = element
-    return new_list
+        return my_list
+    my_list_copy = my_list.copy()
+    my_list_copy[idx] = element
+    return my_list_copy
