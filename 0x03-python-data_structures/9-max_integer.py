5187fd5bbc459675f2b7a27ac8ce910c3cdadeef
SafouaneCh <safouanecharki3@gmail.com>
2024-01-23 04:28:51 -0800

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-if 
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    maxi = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > maxi:
+            maxi = my_list[i]
+    return maxi
