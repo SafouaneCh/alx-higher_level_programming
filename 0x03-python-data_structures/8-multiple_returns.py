52743b06ef2e41bbadd1e775179b63b4e7ee7f94
SafouaneCh <safouanecharki3@gmail.com>
2024-01-23 04:22:48 -0800

    tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def multiple_returns(sentence):
-        if len(sentence) == 0:
-            return None
-        t = len(sentence), sentence[0]
-        return t
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return None
+    t = len(sentence), sentence[0]
+    return t
